331d3f551debff0d170de196157c681de6f647fb
PARTH3612 <parthchoudhari3612@gmail.com>
2024-10-19 15:03:56 +0530

    test3

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/',(req,res)=>{
 })
 
 const corsOptions = {
-    origin: `${BASE_URL}`,
+    origin: `https://lsclient-parth-choudharis-projects.vercel.app/`,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     optionsSuccessStatus: 200,
